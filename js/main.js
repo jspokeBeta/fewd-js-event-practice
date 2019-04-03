afda6306cbef5ec066da1c149080a7ff3ec36c3c
jspokeBeta <47574993+jspokeBeta@users.noreply.github.com>
2019-04-02 20:32:03 -0400

    ButtonOne, Two and Three code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-(function () {
+(function() {
 
-  // your js goes here
+  // Get the first button
+  var buttonOne = document.querySelectorAll('button')[0];
+  // Get the second button
+  var buttonTwo = document.querySelectorAll('button')[1];
+  //Get the third button
+  var buttonThree = document.querySelectorAll('button')[2];
 
 })();
