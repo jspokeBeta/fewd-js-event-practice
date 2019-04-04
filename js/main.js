b3b94dd9d873725a328b4afac4b26c6f8287717f
jspokeBeta <47574993+jspokeBeta@users.noreply.github.com>
2019-04-04 18:22:17 -0400

    Final code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-(function() {
 
-  // Get the first button
-  var buttonOne = document.querySelectorAll('button')[0];
-  // Get the second button
-  var buttonTwo = document.querySelectorAll('button')[1];
-  //Get the third button
-  var buttonThree = document.querySelectorAll('button')[2];
+// Get the first button
+var buttonOne = document.querySelectorAll('button')[0];
+// Get the second button
+var buttonTwo = document.querySelectorAll('button')[1];
+//Get the third button
+var buttonThree = document.querySelectorAll('button')[2];
 
-})();
+//Listen for click events on document
+document.addEventListener('click', function(event) {
+  // only work if the click is on a buttonOne
+  if (!event.target.classList.contains('button-xl')) return;
+  //Click on buttonOne
+  if (event.target === buttonOne) {
+    buttonOne.classList.toggle('button-danger');
+  }
+  if (event.target === buttonTwo) {
+    buttonTwo.innerHTML = 'you clicked me';
+  }
+  if (event.target === buttonThree) {
+    buttonTwo.innerHTML = 'button two';
+  }
+}, false);
